22a598aea0a20a59612f55b2c3941e6f874c6baf
transporteni2026-hue <transporteni2026@gmail.com>
2026-07-13 11:06:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1922,7 +1922,7 @@ try:
                 entrou = st.form_submit_button("▶️ ENTRAR ◀️", use_container_width=True)
                 if entrou:
                     if not tel_is_valid_11(fmt_tel_login):
-                        st.error("Telefone inválido. Use DDD + 9 dígitos (ex: (21) 98765.4321).")
+                        st.error("Telefone inválido. Use DDD + 9 dígitos (ex: 21987654321).")
                     else:
                         tel_login_digits = tel_only_digits(fmt_tel_login)
 
@@ -2103,7 +2103,7 @@ try:
                 if not e_r.strip():
                     st.error("Informe o e-mail cadastrado.")
                 elif not tel_is_valid_11(fmt_tel_rec):
-                    st.error("Telefone inválido. Use DDD + 9 dígitos (ex: (21) 98765.4321).")
+                    st.error("Telefone inválido. Use DDD + 9 dígitos (ex: 21987654321).")
                 else:
                     tel_rec_digits = tel_only_digits(fmt_tel_rec)
 
